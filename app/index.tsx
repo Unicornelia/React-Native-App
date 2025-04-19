2e2fc33e0e7136e48985efa06f89ab103e275cee
Unicornelia <ez.kornelia@gmail.com>
2025-04-19 16:31:01 +0200

    feat: basic start screen for the goal app

--- app/index.tsx
+++ app/index.tsx
@@ -1,18 +1,40 @@
-import {StyleSheet, Text, View} from "react-native";
+import { Button, StyleSheet, TextInput, View } from 'react-native';
 
 export default function Index() {
-    return (
-        <View style={styles.container}>
-            <Text>Edit app/index.tsx to edit this screen.</Text>
-        </View>
-    );
+  return (
+    <View style={styles.container}>
+      <View style={styles.inputContainer}>
+        <TextInput style={styles.textInput} placeholder="Your goals for the day" />
+        <Button title="Add Goal" />
+      </View>
+      <View style={styles.goalsContainer}>
+        <TextInput placeholder="List of goals..." />
+      </View>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: "#fff",
-        justifyContent: "center",
-        alignItems: "center",
-    }
-})
+  container: {
+    padding: 50,
+    flex: 1,
+    backgroundColor: '#ffb8d3',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  inputContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  textInput: {
+    borderWidth: 1,
+    color: '#505050',
+    width: '80%',
+    backgroundColor: '#fff',
+    padding: 5,
+    marginRight: 5,
+  },
+  goalsContainer: {
+    paddingTop: 15,
+  },
+});
